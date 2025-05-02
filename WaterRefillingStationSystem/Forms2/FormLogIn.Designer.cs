c90e6d722e282c35b3776dacf76b961de7d03393
James-Ian09 <jamesianbayonas21@gmail.com>
2025-05-02 21:13:01 +0800

    made some small tweets

--- WaterRefillingStationSystem/Forms2/FormLogIn.Designer.cs
+++ WaterRefillingStationSystem/Forms2/FormLogIn.Designer.cs
@@ -50,9 +50,9 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(-82, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-94, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(573, 473);
+            this.pictureBox1.Size = new System.Drawing.Size(655, 503);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -62,7 +62,7 @@
             this.labelControl1.Appearance.ForeColor = System.Drawing.SystemColors.MenuBar;
             this.labelControl1.Appearance.Options.UseFont = true;
             this.labelControl1.Appearance.Options.UseForeColor = true;
-            this.labelControl1.Location = new System.Drawing.Point(710, 129);
+            this.labelControl1.Location = new System.Drawing.Point(811, 137);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(44, 39);
             this.labelControl1.TabIndex = 1;
@@ -74,7 +74,7 @@
             this.labelControl2.Appearance.ForeColor = System.Drawing.SystemColors.MenuBar;
             this.labelControl2.Appearance.Options.UseFont = true;
             this.labelControl2.Appearance.Options.UseForeColor = true;
-            this.labelControl2.Location = new System.Drawing.Point(606, 166);
+            this.labelControl2.Location = new System.Drawing.Point(693, 176);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(256, 39);
             this.labelControl2.TabIndex = 2;
@@ -84,15 +84,15 @@
             // 
             this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(656, 9);
+            this.pictureBox2.Location = new System.Drawing.Point(750, 10);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(148, 131);
+            this.pictureBox2.Size = new System.Drawing.Size(169, 139);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
             // textEdit3
             // 
-            this.textEdit3.Location = new System.Drawing.Point(598, 218);
+            this.textEdit3.Location = new System.Drawing.Point(683, 232);
             this.textEdit3.Name = "textEdit3";
             this.textEdit3.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(69)))), ((int)(((byte)(95)))));
             this.textEdit3.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
@@ -100,12 +100,12 @@
             this.textEdit3.Properties.Appearance.Options.UseBackColor = true;
             this.textEdit3.Properties.Appearance.Options.UseFont = true;
             this.textEdit3.Properties.Appearance.Options.UseForeColor = true;
-            this.textEdit3.Size = new System.Drawing.Size(259, 38);
+            this.textEdit3.Size = new System.Drawing.Size(296, 38);
             this.textEdit3.TabIndex = 4;
             // 
             // textEdit4
             // 
-            this.textEdit4.Location = new System.Drawing.Point(598, 274);
+            this.textEdit4.Location = new System.Drawing.Point(683, 291);
             this.textEdit4.Name = "textEdit4";
             this.textEdit4.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(69)))), ((int)(((byte)(95)))));
             this.textEdit4.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
@@ -113,7 +113,7 @@
             this.textEdit4.Properties.Appearance.Options.UseBackColor = true;
             this.textEdit4.Properties.Appearance.Options.UseFont = true;
             this.textEdit4.Properties.Appearance.Options.UseForeColor = true;
-            this.textEdit4.Size = new System.Drawing.Size(259, 38);
+            this.textEdit4.Size = new System.Drawing.Size(296, 38);
             this.textEdit4.TabIndex = 5;
             // 
             // simpleButtonLogIn
@@ -128,9 +128,9 @@
             this.simpleButtonLogIn.AppearanceHovered.Options.UseBackColor = true;
             this.simpleButtonLogIn.AppearancePressed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(197)))), ((int)(((byte)(133)))));
             this.simpleButtonLogIn.AppearancePressed.Options.UseBackColor = true;
-            this.simpleButtonLogIn.Location = new System.Drawing.Point(668, 374);
+            this.simpleButtonLogIn.Location = new System.Drawing.Point(763, 397);
             this.simpleButtonLogIn.Name = "simpleButtonLogIn";
-            this.simpleButtonLogIn.Size = new System.Drawing.Size(140, 37);
+            this.simpleButtonLogIn.Size = new System.Drawing.Size(160, 39);
             this.simpleButtonLogIn.TabIndex = 6;
             this.simpleButtonLogIn.Text = "Login";
             this.simpleButtonLogIn.Click += new System.EventHandler(this.simpleButtonLogIn_Click);
@@ -141,9 +141,9 @@
             this.btnPasswordReveal.Appearance.Options.UseBackColor = true;
             this.btnPasswordReveal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnPasswordReveal.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnPasswordReveal.ImageOptions.SvgImage")));
-            this.btnPasswordReveal.Location = new System.Drawing.Point(863, 276);
+            this.btnPasswordReveal.Location = new System.Drawing.Point(986, 293);
             this.btnPasswordReveal.Name = "btnPasswordReveal";
-            this.btnPasswordReveal.Size = new System.Drawing.Size(49, 37);
+            this.btnPasswordReveal.Size = new System.Drawing.Size(56, 39);
             this.btnPasswordReveal.TabIndex = 7;
             this.btnPasswordReveal.Click += new System.EventHandler(this.btnPasswordReveal_Click);
             // 
@@ -152,7 +152,7 @@
             this.hyperlinkLabelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hyperlinkLabelControl1.Appearance.Options.UseFont = true;
             this.hyperlinkLabelControl1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.hyperlinkLabelControl1.Location = new System.Drawing.Point(750, 335);
+            this.hyperlinkLabelControl1.Location = new System.Drawing.Point(857, 356);
             this.hyperlinkLabelControl1.Name = "hyperlinkLabelControl1";
             this.hyperlinkLabelControl1.Size = new System.Drawing.Size(115, 20);
             this.hyperlinkLabelControl1.TabIndex = 8;
@@ -161,23 +161,24 @@
             // 
             // checkEdit1
             // 
-            this.checkEdit1.Location = new System.Drawing.Point(598, 331);
+            this.checkEdit1.Location = new System.Drawing.Point(683, 352);
             this.checkEdit1.Name = "checkEdit1";
             this.checkEdit1.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.checkEdit1.Properties.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(146)))), ((int)(((byte)(160)))));
             this.checkEdit1.Properties.Appearance.Options.UseFont = true;
             this.checkEdit1.Properties.Appearance.Options.UseForeColor = true;
             this.checkEdit1.Properties.Caption = "Remember me ";
-            this.checkEdit1.Size = new System.Drawing.Size(145, 26);
+            this.checkEdit1.Size = new System.Drawing.Size(166, 26);
             this.checkEdit1.TabIndex = 9;
             // 
             // FormLogIn
             // 
             this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(54)))), ((int)(((byte)(80)))));
             this.Appearance.Options.UseBackColor = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 470);
+            this.ClientSize = new System.Drawing.Size(1103, 499);
             this.Controls.Add(this.checkEdit1);
             this.Controls.Add(this.hyperlinkLabelControl1);
             this.Controls.Add(this.btnPasswordReveal);
@@ -188,6 +189,7 @@
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.IconOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("FormLogIn.IconOptions.SvgImage")));
             this.Name = "FormLogIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
