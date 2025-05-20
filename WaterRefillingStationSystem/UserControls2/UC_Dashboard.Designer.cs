3d30d6b38318bb270e0901db556a8ba920e4631c
Pague, Lloyd <kristianpague@gmail.com>
2025-05-20 11:42:59 +0800

    Renamed the ChartConrol into chartControlSales and made a connectionstring in the backend code of it.

--- WaterRefillingStationSystem/UserControls2/UC_Dashboard.Designer.cs
+++ WaterRefillingStationSystem/UserControls2/UC_Dashboard.Designer.cs
@@ -30,21 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.chartControlSales = new DevExpress.XtraCharts.ChartControl();
             this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.salesDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControlSales)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.salesDetailsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
-            this.layoutControl1.Controls.Add(this.chartControl1);
+            this.layoutControl1.Controls.Add(this.chartControlSales);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
@@ -52,6 +52,15 @@
             this.layoutControl1.Size = new System.Drawing.Size(1170, 743);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
+            // 
+            // chartControlSales
+            // 
+            this.chartControlSales.Legend.LegendID = -1;
+            this.chartControlSales.Location = new System.Drawing.Point(16, 16);
+            this.chartControlSales.Name = "chartControlSales";
+            this.chartControlSales.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControlSales.Size = new System.Drawing.Size(1138, 711);
+            this.chartControlSales.TabIndex = 4;
             // 
             // Root
             // 
@@ -63,18 +72,9 @@
             this.Root.Size = new System.Drawing.Size(1170, 743);
             this.Root.TextVisible = false;
             // 
-            // chartControl1
-            // 
-            this.chartControl1.Legend.LegendID = -1;
-            this.chartControl1.Location = new System.Drawing.Point(16, 16);
-            this.chartControl1.Name = "chartControl1";
-            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
-            this.chartControl1.Size = new System.Drawing.Size(1138, 711);
-            this.chartControl1.TabIndex = 4;
-            // 
             // layoutControlItem1
             // 
-            this.layoutControlItem1.Control = this.chartControl1;
+            this.layoutControlItem1.Control = this.chartControlSales;
             this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem1.Name = "layoutControlItem1";
             this.layoutControlItem1.Size = new System.Drawing.Size(1144, 717);
@@ -94,8 +94,8 @@
             this.Size = new System.Drawing.Size(1170, 743);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControlSales)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.salesDetailsBindingSource)).EndInit();
             this.ResumeLayout(false);
@@ -106,7 +106,7 @@
 
         private DevExpress.XtraLayout.LayoutControl layoutControl1;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.XtraCharts.ChartControl chartControlSales;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
         private System.Windows.Forms.BindingSource salesDetailsBindingSource;
     }
--- WaterRefillingStationSystem/UserControls2/UC_Dashboard.cs
+++ WaterRefillingStationSystem/UserControls2/UC_Dashboard.cs
@@ -1,8 +1,11 @@
-﻿using DevExpress.XtraEditors;
+﻿using Dapper;
+using DevExpress.XtraCharts;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,6 +16,7 @@ namespace WaterRefillingStationSystem.UserControls2
 {
     public partial class UC_Dashboard : DevExpress.XtraEditors.XtraUserControl
     {
+        private string _connectionString = @"Data Source=C:\Users\krist\OneDrive\Desktop\OOP Programming\WaterRefillingStationSystem\WaterRefillingStationSystemDB.db;Version=3;";
         public UC_Dashboard()
         {
             InitializeComponent();
