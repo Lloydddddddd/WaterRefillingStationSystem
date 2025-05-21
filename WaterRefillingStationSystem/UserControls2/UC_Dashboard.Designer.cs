358bbfe735adf2d22c5be839e14ba2888300676a
Pague, Lloyd <kristianpague@gmail.com>
2025-05-21 16:54:36 +0800

    Created another chartControl named chartControlDashBoard with 4 bar series with no name yet.

--- WaterRefillingStationSystem/UserControls2/UC_Dashboard.Designer.cs
+++ WaterRefillingStationSystem/UserControls2/UC_Dashboard.Designer.cs
@@ -28,17 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series3 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series4 = new DevExpress.XtraCharts.Series();
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.chartControlDashboard = new DevExpress.XtraCharts.ChartControl();
             this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.salesDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControlDashboard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.salesDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.chartControlDashboard);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
@@ -47,17 +60,49 @@
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
+            // chartControlDashboard
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControlDashboard.Diagram = xyDiagram1;
+            this.chartControlDashboard.Legend.LegendID = -1;
+            this.chartControlDashboard.Location = new System.Drawing.Point(11, 11);
+            this.chartControlDashboard.Name = "chartControlDashboard";
+            series1.Name = "Series 1";
+            series1.SeriesID = 0;
+            series2.Name = "Series 2";
+            series2.SeriesID = 2;
+            series3.Name = "Series 3";
+            series3.SeriesID = 3;
+            series4.Name = "Series 4";
+            series4.SeriesID = 4;
+            this.chartControlDashboard.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1,
+        series2,
+        series3,
+        series4};
+            this.chartControlDashboard.Size = new System.Drawing.Size(1148, 721);
+            this.chartControlDashboard.TabIndex = 4;
+            // 
             // Root
             // 
             this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
             this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
             this.Root.Name = "Root";
+            this.Root.Padding = new DevExpress.XtraLayout.Utils.Padding(8, 8, 8, 8);
             this.Root.Size = new System.Drawing.Size(1170, 743);
             this.Root.TextVisible = false;
             // 
-            // salesDetailsBindingSource
+            // layoutControlItem1
             // 
-            this.salesDetailsBindingSource.DataSource = typeof(WaterRefillingStationSystem.Models.SalesDetails);
+            this.layoutControlItem1.Control = this.chartControlDashboard;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(1154, 727);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
             // 
             // UC_Dashboard
             // 
@@ -67,8 +112,15 @@
             this.Name = "UC_Dashboard";
             this.Size = new System.Drawing.Size(1170, 743);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControlDashboard)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.salesDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -77,6 +129,7 @@
 
         private DevExpress.XtraLayout.LayoutControl layoutControl1;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private System.Windows.Forms.BindingSource salesDetailsBindingSource;
+        private DevExpress.XtraCharts.ChartControl chartControlDashboard;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
     }
 }
